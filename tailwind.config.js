e39d598ea11fa51fde04c3ded0689c7b391d957d
adnan <katmanfatahoni@gmail.com>
2024-09-12 16:33:48 +0700

    animate, index root

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}", "./public/index.html"],
+  content: ["./src/**/*.{html,js}", "./index.html"],
   theme: {
     extend: {
       colors: {
@@ -8,6 +8,9 @@ module.exports = {
         "midnight-aw": "#121063",
         "custom-yellow": "#EBD96B",
         "custom-dark-yellow": "#E5C643",
+        "custom-green": "#C1DCDC",
+        "custom-blue": "#008ECC",
+        "custom-blue-light": "#F3F9FB",
       },
       fontFamily: {
         sans: ["Poppins", "sans-serif"],
